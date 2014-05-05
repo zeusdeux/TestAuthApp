68533279c0c5427cc5b84b003588c9ff5dd74319
Mudit Ameta <zeusdeux@gmail.com>
2014-05-06 01:52:37 +0530

    indentation

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(session({ secret: 'chimichangas my man', key: 'boopityshoopshap'}))
+app.use(session({
+  secret: 'chimichangas my man',
+  key: 'boopityshoopshap'
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -30,10 +33,10 @@ app.use('/search', search);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    console.log(req.url);
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  console.log(req.url);
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -41,23 +44,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
