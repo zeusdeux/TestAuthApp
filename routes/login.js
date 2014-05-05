97a0fee5a2bf9b2ce58e521bba2f6d8623893ff3
Mudit Ameta <zeusdeux@gmail.com>
2014-05-05 17:23:10 +0530

    some comments and formatting

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var UserModel = require('../models/UserModel');
 var crypto = require('crypto');
 
+/* GET login page if not already logged in
+or redirect to / if already loggen in (which inturn redirects to /search) */
 router.get('/', function(req, res, next) {
   var user = new UserModel();
   if (user.find(req.session.uid))
@@ -12,12 +14,15 @@ router.get('/', function(req, res, next) {
   }
 });
 
+/* Handle login */
 router.post('/', function(req, res, next) {
   var user = new UserModel();
   var uid = user.getId(req.param('uname'));
   var userObj;
   if (uid) {
     userObj = user.find(uid);
+
+    //Generate the hash for the password got from user
     var inputPassHash = crypto.createHmac('sha1', new Buffer(userObj.salt,'base64')).update(req.param('passwd')).digest('base64');
     if (userObj.hash === inputPassHash) {
       req.session.regenerate(function() {
--- routes/search.js
+++ routes/search.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var TestModel = require('../models/TestModel');
 var auth = require('./auth').auth;
 
-/* GET test query page. */
+/* GET search page. */
 router.get('/', auth, function(req, res) {
   res.render('search', {results: undefined, q: ''});
 });
 
+/* Respond to search POST request */
 router.post('/', auth, function(req, res){
 	var model = new TestModel();
 	var results = model.find(req.param('q'));
 	res.render('search',{results: results, q: req.param('q')});
 });
-
 
 module.exports = router;
